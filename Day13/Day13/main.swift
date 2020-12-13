7dc9cc21652e98cead5cda1262039b4a82fe73de
Jerry Hsu <jerry.hsu@gc.com>
2020-12-13 08:34:20 -0500

    Encode test cases

--- Day13/Day13/main.swift
+++ Day13/Day13/main.swift
@@ -28,7 +28,8 @@ func isStartTimeAligned(bus: Int, startTime: Int) -> Bool {
     return startTime % bus == 0
 }
 
-func scan(line: String) {
+@discardableResult
+func scan(line: String) -> Int {
     typealias Bus = Int
     typealias Time = Int
     var targetMap = [Bus:Time]()
@@ -41,7 +42,7 @@ func scan(line: String) {
     }
 
     var matchFound = false
-    var test = 0
+    var test = 0 //00_000_000_000_000 / busOrder[0] * busOrder[0]
     while !matchFound {
         matchFound = busOrder.allSatisfy { (bus) -> Bool in
             isStartTimeAligned(bus: bus, startTime: test + targetMap[bus]!)
@@ -51,14 +52,19 @@ func scan(line: String) {
         }
     }
     print(test)
+    return test
 }
 
 func part2() {
-    let line = input .split(separator: "\n").last!
+    let line = input.split(separator: "\n").last!
     scan(line: String(line))
 }
 
+print(scan(line: "7,13,x,x,59,x,31,19") == 1068781)
+print(scan(line: "17,x,13,19") == 3417)
+print(scan(line: "67,7,59,61") == 754018)
+print(scan(line: "67,x,7,59,61") == 779210)
+print(scan(line: "67,7,x,59,61") == 1261476)
+print(scan(line: "1789,37,47,1889") == 1202161486)
 part2()
-scan(line: "17,x,13,19")
-scan(line: "1789,37,47,1889")
 
