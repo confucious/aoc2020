27b1e658b6fa0ae4cd908b56e6ae4fb23db69324
Jerry Hsu <jerry.hsu@gc.com>
2020-12-23 08:45:47 -0500

    Day23 part2 done.

--- Day23/Day23/main.swift
+++ Day23/Day23/main.swift
@@ -8,34 +8,80 @@
 import Foundation
 
 
-let input = "389125467"
+let input = "925176834"
+
+class Element {
+    let value: Int
+    var next: Element!
+    var prev: Element!
+
+    init(value: Int) {
+        self.value = value
+    }
+}
+
 
 var cups = input.map { Int(String($0))! } + (10...1000000)
 let maxValue = cups.max()!
-for index in 0..<10_000 {
-    let current = cups[0]
-    let pickUp = cups[1...3]
-    let remaining = Array(cups[4...])
-    var destinationIndex: Int!
+let elements = cups.map { Element.init(value: $0) }
+var current = elements[0]
+
+func safeIndex(_ index: Int) -> Int {
+    return (index + maxValue) % maxValue
+}
+
+func display(_ element: Element) {
+    var iterator = current
+
+    print((0..<9).reduce(into: "", { (result, _) in
+        result += String(iterator.value)
+        iterator = iterator.next
+    }))
+}
+elements.enumerated().forEach { (index, element) in
+    element.next = elements[safeIndex(index + 1)]
+    element.prev = elements[safeIndex(index - 1)]
+}
+
+var indexed = Array<Element>(repeating: Element(value: 0), count: maxValue)
+elements.forEach { (element) in
+    indexed[element.value - 1] = element
+}
+
+for index in 0..<10_000_000 {
+    let pickUp = [current.next!, current.next.next!, current.next.next.next!]
+    let pickUpValues = pickUp.map { $0.value }
+    let remaining = current.next.next.next.next!
+    var search = remaining
     for offset in 1...4 {
-        let target = current - offset < 1
-            ? current - offset + maxValue
-            : current - offset
-//          print("\(current) \(target)")
-        if pickUp.contains(target) {
+        let targetValue = (current.value - offset < 1)
+            ? current.value - offset + maxValue
+            : current.value - offset
+//        print("\(current.value) \(targetValue)")
+        if pickUpValues.contains(targetValue) {
             continue
         }
-        destinationIndex = remaining.firstIndex { (value) -> Bool in
-            value == target
-        }
+
+        search = indexed[targetValue - 1]
+//        while search.value != targetValue {
+//            search = search.next
+//        }
         break
     }
-    cups = remaining[...destinationIndex]
-        + pickUp
-        + remaining[(destinationIndex + 1)...]
-        + [current]
-//    print(cups)
+    current.next = remaining
+    remaining.prev = current
+
+    let searchNext = search.next!
+    search.next = pickUp[0]
+    pickUp[0].prev = search
+    pickUp[2].next = searchNext
+    searchNext.prev = pickUp[2]
+
+    current = current.next
+
     if index % 1000000 == 0 { print(index) }
+//    display(current)
 }
-let index = cups.firstIndex(of: 1)!
-print(cups[index + 1] * cups[index + 2])
+
+print("\(indexed[0].value) \(indexed[0].next.value) \(indexed[0].next.next.value)")
+print("\(indexed[0].next.value * indexed[0].next.next.value)")
