8338b4345abd2f95ee1502bb99c743f47002470d
Jerry Hsu <jerry.hsu@gc.com>
2020-12-24 09:31:42 -0500

    Day24 part2 done.

--- Day24/Day24/main.swift
+++ Day24/Day24/main.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum Direction {
+enum Direction: CaseIterable {
     case e
     case w
     case nw
@@ -89,3 +89,61 @@ func part1() {
 }
 
 part1()
+
+func part2() {
+    var blackTiles = Set<HexPoint>()
+    var minRow = 0
+    var maxRow = 0
+    var minCol = 0
+    var maxCol = 0
+
+    let points = input.components(separatedBy: "\n")
+        .map(parse)
+        .map {
+            $0.reduce(HexPoint(row: 0, col: 0), { (previous, direction) in
+                previous.point(to: direction)
+            })
+        }
+
+    points.forEach { (point) in
+        minRow = min(point.row, minRow)
+        maxRow = max(point.row, maxRow)
+        minCol = min(point.col, minCol)
+        maxCol = max(point.col, maxCol)
+        if blackTiles.contains(point) {
+            blackTiles.remove(point)
+        } else {
+            blackTiles.insert(point)
+        }
+    }
+
+    print("Start \(blackTiles.count)")
+
+    for iteration in 1...100 {
+        let previousState = blackTiles
+        for row in (minRow - 1)...(maxRow + 1) {
+            for col in (minCol - 1)...(maxCol + 1) {
+                let point = HexPoint(row: row, col: col)
+                let count = Direction.allCases.reduce(0, { (sum, direction) in
+                    sum + (previousState.contains(point.point(to: direction)) ? 1 : 0)
+                })
+                if previousState.contains(point) {
+                    if count == 0 || count > 2 {
+                        blackTiles.remove(point)
+                    }
+                } else {
+                    if count == 2 {
+                        blackTiles.insert(point)
+                        minRow = min(point.row, minRow)
+                        maxRow = max(point.row, maxRow)
+                        minCol = min(point.col, minCol)
+                        maxCol = max(point.col, maxCol)
+                    }
+                }
+            }
+        }
+        print("\(iteration) \(blackTiles.count)")
+    }
+}
+
+part2()
